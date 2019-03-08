7a909e0a5fe51d7957bc3fb6fd32339453339238
minjiang93 <42121141+minjiang93@users.noreply.github.com>
2019-03-08 15:30:13 -0800

    Add files via upload

---
+++ PosTestJunit.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PosTestJunit {
+	
+   String message = "Hello World";	
+   MessageUtil messageUtil = new MessageUtil(message);
+
+   @Test
+   public void testPrintMessage() {
+      assertEquals(message,messageUtil.printMessage());
+   }
+}
---
+++ SuiteTest.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   NegTestJunit.class,
+   PosTestJunit.class
+})
+
+public class SuiteTest {
+
+}
